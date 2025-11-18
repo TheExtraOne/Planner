8032ea9f169e50118afb358c63a3915390125077
KateMThrive <kate.mikhasiova@partnerwiththrive.com>
2025-11-18 23:22:38 +0300

    fix: import

--- src/pages/NotFoundPage/NotFound.tsx
+++ src/pages/NotFoundPage/NotFound.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from 'src/pages/NotFound.module.css';
+import styles from 'src/pages/NotFoundPage/NotFound.module.css';
 
 const NotFound: React.FC = () => {
   const navigate = useNavigate();
